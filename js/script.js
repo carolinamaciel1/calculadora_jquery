0e0141e2ebb3b9d5b01cb119df98d503a220c403
dev14 <dev14@dev14.localdomain>
2018-09-25 11:20:03 -0300

    Primeira versão, códigos sem javascript, somente html e css

---
+++ js/script.js
@@ -0,0 +1,46 @@
+$('#ce').click(function(){
+    $('#campo1').val(''); 
+});
+
+$('#zero').click(function(){
+    $('#campo1').val('0'); 
+});
+
+$('#um').click(function(){
+    $('#campo1').val('1'); 
+});
+
+$('#dois').click(function(){
+    $('#campo1').val('2'); 
+});
+
+$('#tres').click(function(){
+    $('#campo1').val('3'); 
+});
+
+$('#quatro').click(function(){
+    $('#campo1').val('4'); 
+});
+
+$('#cinco').click(function(){
+    $('#campo1').val('5'); 
+});
+
+$('#seis').click(function(){
+    $('#campo1').val('6'); 
+});
+
+$('#sete').click(function(){
+    $('#campo1').val('7'); 
+});
+
+$('#oito').click(function(){
+    $('#campo1').val('8'); 
+});
+
+$('#nove').click(function(){
+    $('#campo1').val('9'); 
+});
+
+
+
