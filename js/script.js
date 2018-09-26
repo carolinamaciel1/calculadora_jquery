823b4ac7803d9ce2572b65e2357d2c12ea1f26e5
carolina maciel <carolinamaciel37@gmail.com>
2018-09-26 00:36:55 -0300

    segunda versao calculadora

--- js/script.js
+++ js/script.js
@@ -1,46 +1,82 @@
-$('#ce').click(function(){
-    $('#campo1').val(''); 
+$(document).ready(function(){
+       
+       var v1, v2, op;
+
+       $('.buttontecla').click(function(){
+        $('#campo1').val($('#campo1').val() + $(this).val());
+       });
+
+       $('#ce').click(function(){
+            $('#campo1').val('');
+       });
+
+
+       //script para efetuar a soma
+       $('#soma').click(function(){
+            if($('#campo1').val() != ''){
+                v1 = parseFloat($('#campo1').val());
+                $('#campo1').val('');
+                op = "soma";
+                $("#campo1").text($(this).val());
+                //alert(op);
+            }else{
+                alert("Insira um valor para efetuar a operação!");
+            }
+       });
+
+
+       //script para efetuar a sub
+       $('#sub').click(function(){
+        if($('#campo1').val != ''){
+            v1 = parseFloat($('#campo1').val());
+            $('#campo1').val('');
+            op = "sub";
+            $('#campo1').text($(this).val());
+        }else{
+            alert('Insira um valor para efetuar a operação!');
+        }
+       });
+
+       //script para efetuar a mult
+       $('#mult').click(function(){
+        if($('#campo1').val != ''){
+            v1 = parseFloat($('#campo1').val());
+            $('#campo1').val('');
+            op = "mult";
+            $('#campo1').text($(this).val());
+        }else{
+            alert('Insira um valor para efetuar a operação!');
+        }
+       });
+
+        //script para efetuar a div
+       $('#div').click(function(){
+        if($('#campo1').val != ''){
+            v1 = parseFloat($('#campo1').val());
+            $('#campo1').val('');
+            op = "div";
+            $('#campo1').text($(this).val());
+        }else{
+            alert('Insira um valor para efetuar a operação!');
+        }
+       });
+
+        $('#igual').click(function(){
+            if($('#campo1').val() != ''){
+                v2 = parseFloat($('#campo1').val());
+                $('#campo1').val('');
+                if(op == "soma"){
+                    $('#campo1').val(v1+v2);
+                } else if(op == "sub"){
+                    $('#campo1').val(v1-v2);
+                }else if(op == "mult"){
+                    $('#campo1').val(v1*v2);
+                }else if(op == "div"){
+                    $('#campo1').val(v1/v2);
+                }
+            }else{
+                alert("Insira um valor para efetuar o cálculo!");
+            }
+       });
 });
-
-$('#zero').click(function(){
-    $('#campo1').val('0'); 
-});
-
-$('#um').click(function(){
-    $('#campo1').val('1'); 
-});
-
-$('#dois').click(function(){
-    $('#campo1').val('2'); 
-});
-
-$('#tres').click(function(){
-    $('#campo1').val('3'); 
-});
-
-$('#quatro').click(function(){
-    $('#campo1').val('4'); 
-});
-
-$('#cinco').click(function(){
-    $('#campo1').val('5'); 
-});
-
-$('#seis').click(function(){
-    $('#campo1').val('6'); 
-});
-
-$('#sete').click(function(){
-    $('#campo1').val('7'); 
-});
-
-$('#oito').click(function(){
-    $('#campo1').val('8'); 
-});
-
-$('#nove').click(function(){
-    $('#campo1').val('9'); 
-});
-
-
 
